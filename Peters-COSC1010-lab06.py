82ff39b01eb88af8147ea6f89f1161a50206fd3d
GitHubGal2000 <colinapeters06@outlook.com>
2024-10-16 22:03:14 +0000

    finished

--- Peters-COSC1010-lab06.py
+++ Peters-COSC1010-lab06.py
@@ -1,13 +1,12 @@
-# Your Name Here
+# Colin Peters
 # UWYO COSC 1010
 # Submission Date
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 15
+# Sources, people worked with, help given to: Colton, Jed
 # your
 # comments
 # here
-
 
 random_string = """
 jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
@@ -79,22 +78,61 @@ print(len(random_string)) # Print out the size for reference
     # You will  need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
 
-
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
 
+occurance_dict = {}
+
+most = 0
+most_letters = ()
+least = 100000
+least_letters = ()
+for letter, value in occurance_dict.items():
+    if value > most:
+        most = value
+        most_letters = letter
+    if value < least:
+        least = value
+        least_letters = letter
+
+
+
 # Output: each letter and its corresponding occurrence in alphabetical order
+
+for letter in sorted(set(random_string)):
+    occurance_dict[letter] = 0
+
+for letter in random_string:
+    occurance_dict[letter] += 1
+
+for letter, value in occurance_dict.items():
+    print(f"letter: {letter}")
+    print(f"value: {value}"\n)
+
+most = 0
+most_letters = ()
+least = 100000
+least_letters = ()
+for letter, value in occurance_dict.items():
+    if value > most:
+        most = value
+        most_letters = letter
+    if value < least:
+        least = value
+        least_letters = letter
 
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
-
-print(f"The letter that occurred the most is {most_occurred}")
+print(f"The letter that occurred the most is {most_letters}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+
+print(f"The letter that occurred the least is {least_letters}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+for letter, value in occurance_dict.items():
+    print(f"letter: {letter}")
+    print(f"percent: {value/25}\n")
